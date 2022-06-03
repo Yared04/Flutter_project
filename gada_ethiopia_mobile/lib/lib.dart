a9ede54a03a8367b38fff9577f065a73c0e60f73
kaleabe-n <nkaleabe001@gmail.com>
2022-06-03 14:17:23 +0300

    donations fixed

--- gada_ethiopia_mobile/lib/lib.dart
+++ gada_ethiopia_mobile/lib/lib.dart
@@ -2,3 +2,4 @@ export 'widgets/home.dart';
 export 'infrastructure/infrastracture.dart';
 export 'domain/domain.dart';
 export 'presentation/presentation.dart';
+export 'application/application.dart';
